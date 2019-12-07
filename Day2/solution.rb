aea30ba8b8fc4bd42e97c23a690b4242133ed5e0
Maryam Kaka <maryam.kaka@shopify.com>
2019-12-07 11:24:33 -0500

    Day 2 - Part 2

--- Day2/solution.rb
+++ Day2/solution.rb
@@ -20,9 +20,26 @@ puts "Case 2: #{process_instructions([2,3,0,3,99])}"
 puts "Case 3: #{process_instructions([2,4,4,5,99,0])}"
 puts "Case 4: #{process_instructions([1,1,1,4,99,5,6,0,99])}"
 
+data = File.read('input.txt').split(',').map{|x| x.to_i}.freeze
+
 # Part 1
-data = File.read('input.txt').split(',').map{|x| x.to_i}
-data[1] = 12
-data[2] = 2
-data = process_instructions(data)
-puts "Answer: #{data[0]}"
+instructions = data.dup
+instructions[1] = 12
+instructions[2] = 2
+instructions = process_instructions(instructions)
+puts "Answer: #{instructions[0]}"
+
+# Part 2
+attempts = 99
+attempts.times do |n|
+  attempts.times do |v|
+    instructions = data.dup
+    instructions[1] = n
+    instructions[2] = v
+
+    if process_instructions(instructions)[0] == 19690720
+      puts "Part 2: Answer: #{100 * n + v}"
+      break
+    end
+  end
+end
