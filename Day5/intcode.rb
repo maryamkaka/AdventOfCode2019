2a969108e66914ee32a5c9facc99ec822a02451e
Maryam Kaka <maryam.kaka@shopify.com>
2020-01-04 21:26:31 -0500

    Actually read instructions properly this time:

--- Day5/intcode.rb
+++ Day5/intcode.rb
@@ -1,50 +1,61 @@
 class Intcode
+  attr_reader :outputs, :memory
+
   def initialize(instructions)
-    @memory = instructions
+    @memory = instructions.dup
     @pointer = 0
+    @outputs =[]
   end
 
-  def run(inputs = nil, outputs = nil)
+  def run(input = nil)
+    @input = input
+
     while pointer != nil
       process_instructions
     end
 
-    memory
+    self
   end
 
   private
 
-  attr_accessor :pointer, :memory
+  attr_writer :outputs, :memory
+  attr_accessor :pointer
 
   def process_instructions()
-    instruction = memory[pointer]
+    rasie "Something bad happened. Invalid instruction" if @pointer > memory.length
 
-    if instruction == 99
-      @pointer = nil
-      return
-    end
-
-    opcode, pos_mode1, pos_mode2, pos_mode3 = instruction.to_s.reverse.chars.map(&:to_i)
-    param1 = pos_mode1 == 1 ? memory[pointer + 1] : memory[memory[pointer + 1]]
-    param2 = pos_mode2 == 1 ? memory[pointer + 2] : memory[memory[pointer + 2]]
-    param3 = pos_mode3 == 1 ? memory[pointer + 3] : memory[memory[pointer + 3]]
-
-    puts "DE: #{opcode} C: #{param1}  B: #{param2}  A: #{param3} "
+    instruction = memory[pointer].to_s.chars
+    opcode = instruction.pop(2).join.to_i
+    mode1, mode2, mode3 = instruction.map(&:to_i)
 
     case opcode
     when 1
-      memory[memory[pointer + 3]] = param1 + param2
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) + get_parameter_value(2, mode2)
       @pointer += 4
     when 2
-      memory[memory[pointer + 3]] = param1 * param2
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) * get_parameter_value(2, mode2)
       @pointer += 4
     when 3
       dest_pointer = memory[pointer + 1]
-      memory[dest_pointer] = inputs
+      memory[dest_pointer] = @input
+      @pointer +=2
+    when 4
+      outputs.push(get_parameter_value(1, mode1))
+      @pointer +=2
+    when 99
+      @pointer = nil
+      return
     else
       @pointer = nil
       raise "Unexpected opcode"
     end
+  end
+
+  def get_parameter_value(param_idx, mode = nil)
+    mode != nil && mode == 1 ? memory[pointer + param_idx] : memory[memory[pointer + param_idx]]
   end
 
 end
--- Day5/solution.rb
+++ Day5/solution.rb
@@ -1,12 +1,15 @@
 require_relative "Intcode"
 
 # Part 1
-puts "Case 1: #{Intcode.new([1,0,0,0,99]).run}, Expected: [2, 0, 0, 0, 99]"
-puts "Case 2: #{Intcode.new([2,3,0,3,99]).run}, Expected: [2, 3, 0, 6, 99]"
-puts "Case 3: #{Intcode.new([2,4,4,5,99,0]).run}, Expected: [2, 4, 4, 5, 99, 9801]"
-puts "Case 4: #{Intcode.new([1,1,1,4,99,5,6,0,99]).run}, Expected: [30, 1, 1, 4, 2, 5, 6, 0, 99]"
+puts "Case 1: #{Intcode.new([1,0,0,0,99]).run.memory}, Expected: [2, 0, 0, 0, 99]"
+puts "Case 2: #{Intcode.new([2,3,0,3,99]).run.memory}, Expected: [2, 3, 0, 6, 99]"
+puts "Case 3: #{Intcode.new([2,4,4,5,99,0]).run.memory}, Expected: [2, 4, 4, 5, 99, 9801]"
+puts "Case 4: #{Intcode.new([1,1,1,4,99,5,6,0,99]).run.memory}, Expected: [30, 1, 1, 4, 2, 5, 6, 0, 99]"
+puts "Case 4: #{Intcode.new([1101,100,-1,4,0]).run.memory}, Expected: [1101, 100, -1, 4, 99]"
+puts "Case 5: #{Intcode.new([3,0,4,0,99]).run(30).outputs}, Expected: [30]"
 
 data = File.read('input.txt').split(',').map{|x| x.to_i}.freeze
+# puts "Answer: #{Intcode.new(data).run(1)}"
 
 
 # Part 2
