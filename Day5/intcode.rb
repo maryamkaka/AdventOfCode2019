88b53f12c75f10491fdf7fbef7b9e5973c29c8b6
Maryam <maryamkaka@users.noreply.github.com>
2020-01-05 10:25:57 -0500

    Day 5: Sunny with a Chance of Asteroids (#3)
    - Rewrote Intcode to handle immediate and positional mode instructions 
    - Added new instruction codes

---
+++ Day5/intcode.rb
@@ -0,0 +1,75 @@
+class Intcode
+  attr_reader :outputs, :memory
+
+  def initialize(instructions)
+    @memory = instructions.dup
+    @pointer = 0
+    @outputs =[]
+  end
+
+  def run(input = nil)
+    @input = input
+
+    while pointer != nil
+      process_instructions
+    end
+
+    self
+  end
+
+  private
+
+  attr_writer :outputs, :memory
+  attr_accessor :pointer
+
+  def process_instructions()
+    rasie "Something bad happened. Invalid instruction" if @pointer > memory.length
+
+    instruction = memory[pointer].to_s.chars
+    opcode = instruction.pop(2).join.to_i
+    mode1, mode2, mode3 = instruction.reverse.map(&:to_i)
+
+    case opcode
+    when 1
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) + get_parameter_value(2, mode2)
+      @pointer += 4
+    when 2
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) * get_parameter_value(2, mode2)
+      @pointer += 4
+    when 3
+      raise "Missing input" if @input == nil
+
+      dest_pointer = memory[pointer + 1]
+      memory[dest_pointer] = @input
+      @pointer +=2
+    when 4
+      outputs.push(get_parameter_value(1, mode1))
+      @pointer +=2
+    when 5
+      @pointer = get_parameter_value(1, mode1) != 0 ? get_parameter_value(2, mode2) : @pointer + 3
+    when 6
+      @pointer = get_parameter_value(1, mode1) == 0 ? get_parameter_value(2, mode2) : @pointer + 3
+    when 7
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) < get_parameter_value(2, mode2) ? 1 : 0
+      @pointer += 4
+    when 8
+      dest_pointer = memory[pointer + 3]
+      memory[dest_pointer] = get_parameter_value(1, mode1) == get_parameter_value(2, mode2) ? 1 : 0
+      @pointer += 4
+    when 99
+      @pointer = nil
+      return
+    else
+      @pointer = nil
+      raise "Unexpected opcode"
+    end
+  end
+
+  def get_parameter_value(param_idx, mode = nil)
+    mode != nil && mode == 1 ? memory[pointer + param_idx] : memory[memory[pointer + param_idx]]
+  end
+
+end
---
+++ Day5/solution.rb
@@ -0,0 +1,37 @@
+require_relative "Intcode"
+
+# Part 1
+puts " ----- Part 1 ----- "
+puts "Case 1: #{Intcode.new([1,0,0,0,99]).run.memory}, Expected: [2, 0, 0, 0, 99]"
+puts "Case 2: #{Intcode.new([2,3,0,3,99]).run.memory}, Expected: [2, 3, 0, 6, 99]"
+puts "Case 3: #{Intcode.new([2,4,4,5,99,0]).run.memory}, Expected: [2, 4, 4, 5, 99, 9801]"
+puts "Case 4: #{Intcode.new([1,1,1,4,99,5,6,0,99]).run.memory}, Expected: [30, 1, 1, 4, 2, 5, 6, 0, 99]"
+puts "Case 4: #{Intcode.new([1101,100,-1,4,0]).run.memory}, Expected: [1101, 100, -1, 4, 99]"
+puts "Case 5: #{Intcode.new([3,0,4,0,99]).run(30).outputs}, Expected: [30]"
+
+data = File.read('input.txt').split(',').map{|x| x.to_i}.freeze
+puts "Answer: #{Intcode.new(data).run(1).outputs}"
+
+# Part 2
+puts " ----- Part 2 ---- "
+puts "Case 7 (input equals 8): #{Intcode.new([3,9,8,9,10,9,4,9,99,-1,8]).run(8).outputs}, Expected: [1]"
+puts "Case 6 (input equals 8): #{Intcode.new([3,9,8,9,10,9,4,9,99,-1,8]).run(20).outputs}, Expected: [0]"
+puts "Case 8 (input less than 8): #{Intcode.new([3,9,7,9,10,9,4,9,99,-1,8]).run(1).outputs}, Expected: [1]"
+puts "Case 9 (input less than 8): #{Intcode.new([3,9,7,9,10,9,4,9,99,-1,8]).run(20).outputs}, Expected: [0]"
+puts "Case 10 (input equals 8): #{Intcode.new([3,3,1108,-1,8,3,4,3,99]).run(8).outputs}, Expected: [1]"
+puts "Case 11 (input equals 8): #{Intcode.new([3,3,1108,-1,8,3,4,3,99]).run(20).outputs}, Expected: [0]"
+puts "Case 12 (input less than 8): #{Intcode.new([3,3,1107,-1,8,3,4,3,99]).run(1).outputs}, Expected: [1]"
+puts "Case 13 (input less than 8): #{Intcode.new([3,3,1107,-1,8,3,4,3,99]).run(8).outputs}, Expected: [0]"
+puts "Case 14 (jump test, position mode): #{Intcode.new([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run(8).outputs}, Expected: [1]"
+puts "Case 15 (jump test, position mode): #{Intcode.new([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]).run(0).outputs}, Expected: [0]"
+
+larger_example = [
+  3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+  1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+  999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99,
+]
+puts "Case 16: #{Intcode.new(larger_example).run(6).outputs}, Expected: [999]"
+puts "Case 17: #{Intcode.new(larger_example).run(8).outputs}, Expected: [1000]"
+puts "Case 18: #{Intcode.new(larger_example).run(10).outputs}, Expected: [1001]"
+
+puts "Answer: #{Intcode.new(data).run(5).outputs}"
