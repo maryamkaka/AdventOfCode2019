ca8875121100ca1290deaa5821265b0aed43a552
Maryam <maryamkaka@users.noreply.github.com>
2020-01-05 16:07:31 -0500

    Day 6: Universal Orbit Map --- Part 2 (#5)

--- Day6/solution.rb
+++ Day6/solution.rb
@@ -1,6 +1,5 @@
 require_relative "orbit_map.rb"
 
-
 file = File.open('input.txt')
 data = file.readlines.map(&:chomp).freeze
 file.close
@@ -8,3 +7,8 @@ file.close
 puts " ----- Part 1 ----- "
 puts("Case 1: #{OrbitMap.new([ "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]).checksum}, Expected: 42")
 puts "Answer: #{OrbitMap.new(data).checksum}"
+
+puts " ----- Part 2 ----- "
+test_data = [ "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"]
+puts "Case 1: #{OrbitMap.new(test_data).orbital_transfer_distance("YOU", "SAN")}, Expected: 4"
+puts "Answer: #{OrbitMap.new(data).orbital_transfer_distance("YOU", "SAN")}"
