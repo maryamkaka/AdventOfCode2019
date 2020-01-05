d1d5ff1156f81482313ef264f922b5a7ae4edb37
Maryam Kaka <maryam.kaka@shopify.com>
2020-01-05 14:20:55 -0500

    Got example to work

---
+++ Day6/orbit_map.rb
@@ -0,0 +1,74 @@
+class OrbitMap
+  attr_reader :checksum, :objects
+
+  def initialize(map)
+    @objects = []
+    read_map(map)
+    @checksum = get_checksum
+  end
+
+  private
+
+  attr_accessor :objects
+
+  def read_map(map)
+    map.each do | orbital |
+      orbitting_body_name, body_name = orbital.split(")")
+
+      orbitting_body = get_body(orbitting_body_name)
+      if(orbitting_body == nil)
+        orbitting_body = Body.new(orbitting_body_name)
+        objects.push(orbitting_body)
+      end
+
+      body = get_body(body_name)
+      if(body == nil)
+        objects.push(Body.new(body_name, orbitting_body))
+      else
+        body.set_orbitting_body(orbitting_body)
+      end
+    end
+  end
+
+  def get_body(name)
+    objects.find { | body | body.name == name }
+  end
+
+  def get_checksum
+    @objects.reduce(0) do | count, obj |
+      if(obj == nil || obj.orbit_count == nil)
+        binding.pry
+      end
+      count += obj.orbit_count
+    end
+  end
+end
+
+class Body
+  attr_reader :name, :orbitting_body, :orbit_count
+
+  def initialize(name, orbitting_body = nil)
+    @orbit_count = 0
+    @name = name
+    set_orbitting_body(orbitting_body) if orbitting_body != nil
+  end
+
+  def set_orbitting_body(new_body)
+    @orbitting_body = new_body
+    @orbit_count = count_orbits
+  end
+
+  private
+
+  def count_orbits
+    count = 0
+
+    orbit = orbitting_body
+    while orbit != nil
+      count += 1
+      orbit = orbit.orbitting_body
+    end
+
+    count
+  end
+end
